c640d034d749ca18cb54bd084fae6e29af809013
Henrique Alves Moreno <henrique.moreno@dynatest.com.br>
2025-12-29 17:19:07 -0300

    Voltando a funcionar o /ready

--- Program.cs
+++ Program.cs
@@ -43,8 +43,19 @@ var appStartTime = DateTime.UtcNow;
 
 app.MapGet("/ready", () =>
 {
- return Results.StatusCode(503);
-;
+    // Simula warmup (ex: conexões, cache, migrations etc.)
+    var uptime = DateTime.UtcNow - appStartTime;
+
+    if (uptime.TotalSeconds < 5)
+    {
+        return Results.StatusCode(503);
+    }
+
+    return Results.Ok(new
+    {
+        status = "ready",
+        service = "ApiDemo"
+    });
 });
 
 
